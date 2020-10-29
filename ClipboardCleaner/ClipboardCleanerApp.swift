4f2479688a82eaf55db443f9379751fd6a665b85
Ma Xueyuan <machinemxy@me.com>
2020-10-29 23:18:14 +0900

    add project

---
+++ ClipboardCleaner/ClipboardCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardCleanerApp.swift
+//  ClipboardCleaner
+//
+//  Created by Ma Xueyuan on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
